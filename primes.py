8638697f27e498b26b14c86c7dfbefe86c84cfad
Mateo Marthoz Blanco <mateo.marthoz_blanco@kcl.ac.uk>
2024-10-18 04:52:40 +0100

    Implement primes function

--- primes.py
+++ primes.py
@@ -1,6 +1,19 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 def primes(number_of_primes):
-    list = []
+    prime_list = []
+    number = 2
+    while len(prime_list) < number_of_primes:
+        if is_prime(number):
+            prime_list.append(number)
+        number += 1
     return list

8ab5f3dc04a573b305626b926f686d96a3752f8a
Mateo Marthoz Blanco <mateo.marthoz_blanco@kcl.ac.uk>
2024-10-18 05:16:28 +0100

    Correct 'return list' to 'return prime_list'

--- primes.py
+++ primes.py
@@ -4,7 +4,9 @@
 def is_prime(n):
     if n <= 1:
         return False
-    for i in range(2, int(n ** 0.5) + 1):
+    if n == 2:
+        return True
+    for i in range(3, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
     return True
@@ -16,4 +18,4 @@ def primes(number_of_primes):
         if is_prime(number):
             prime_list.append(number)
         number += 1
-    return list
+    return prime_list
